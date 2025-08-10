e9272db3147e4f402bfc37f46db79dbd193f968b
Sujitha R <sujitha.r@s.amity.edu>
2025-08-10 14:50:45 +0530

    C7

--- test.cpp
+++ test.cpp
@@ -18,4 +18,26 @@ void clearInput() {
 int main() {
     double radius;
     int choice;
+do {
+        cout << "\n=== Circle Calculator ===\n";
+        cout << "1. Calculate Area\n";
+        cout << "2. Calculate Circumference\n";
+        cout << "3. Calculate Both\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice (1-4): ";
+        cin >> choice;
+
+        if (cin.fail() || choice < 1 || choice > 4) {
+            cout << "Invalid input! Please enter a number between 1 and 4.\n";
+            clearInput();
+            continue;
+        }
+
+        if (choice == 4) {
+            cout << "Exiting the program. Goodbye!\n";
+            break;
+        }
+
+        cout << "Enter the radius of the circle: ";
+        cin >> radius;
 
